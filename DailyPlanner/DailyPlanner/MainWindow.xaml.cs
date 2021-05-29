ff7ec79c85beadfc7b96ec21f774c8b042c3ca8d
Ragnyx47 <jakub3389@gmail.com>
2021-05-29 18:47:14 +0200

    #1 Initialization of projects

---
+++ DailyPlanner/DailyPlanner/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DailyPlanner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 5; i++)
+            {
+                TaskView g = new TaskView();
+                Frame fr = new Frame();
+                fr.Content = g;
+                panelForTasks.Children.Add(fr);
+
+            }
+        }
+
+        private void Calendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            System.Collections.IList addedItems = e.AddedItems;
+            System.Collections.IList removedItems = e.RemovedItems;
+            string kk = "dasdsad";
+        }
+    }
+}
